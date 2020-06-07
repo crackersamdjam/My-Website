161b0c18e23638a8b649b3f786e1d4c1ab37fb25
Eric <crackersamdjam@gmail.com>
2020-06-07 10:21:04 -0400

    edit cpp_to_html script

--- assets/scripts/cpp_to_html.cpp
+++ assets/scripts/cpp_to_html.cpp
@@ -1,29 +1,11 @@
 #include <bits/stdc++.h>
-#define all(x) (x).begin(), (x).end()
-#define gc getchar()
-#define pc(x) putchar(x)
-template<typename T> void scan(T &x){x = 0;bool _=0;T c=gc;_=c==45;c=_?gc:c;while(c<48||c>57)c=gc;for(;c<48||c>57;c=gc);for(;c>47&&c<58;c=gc)x=(x<<3)+(x<<1)+(c&15);x=_?-x:x;}
-template<typename T> void printn(T n){bool _=0;_=n<0;n=_?-n:n;char snum[65];int i=0;do{snum[i++]=n%10+48;n/= 10;}while(n);--i;if (_)pc(45);while(i>=0)pc(snum[i--]);}
-template<typename First, typename ... Ints> void scan(First &arg, Ints&... rest){scan(arg);scan(rest...);}
-template<typename T> void print(T n){printn(n);pc(10);}
-template<typename First, typename ... Ints> void print(First arg, Ints... rest){printn(arg);pc(32);print(rest...);}
 
 using namespace std;
 
-void init(){
-    #ifndef ONLINE_JUDGE
-    freopen("in.txt", "r", stdin);
-    freopen("out.txt", "w", stdout);
-    freopen("err.txt", "w", stderr);
-    #endif
-}
+string s;
 
 int main(){
-    init();
-    cin.sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    string s;
+	
     while(getline(cin, s)){
         for(const char &c: s){
             if(c == '<')
@@ -35,7 +17,6 @@ int main(){
         }
         cout<<'\n';
     }
-
     
     return 0;
 }
